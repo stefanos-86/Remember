6a1af38817ac7ee0fb16928ee89aadfbb7521ce6
stefano <you@example.com>
2017-11-10 22:20:38 +0100

    Extract common part of test scripts.

--- CreateTestCores.py
+++ CreateTestCores.py
@@ -3,6 +3,8 @@ import shutil
 import subprocess
 import time
 
+import TestSuite as ts
+
 # This scripts automates the creation of the test core dumps.
 #
 # It relies on the "conventional" position and content of the CMakeLists.txt to find the programs.
@@ -16,49 +18,28 @@ import time
 # It is bad parallel programming, but it works well enough in practice.
 
 
-CORES_FOLDER = os.path.abspath("testCores")
-TEST_PROGRAMS_FOLDER = os.path.abspath("testPrograms")
-
-
 def clean_up_core_folder():
     """Erases and re-create any pre-existing folder for the core files."""
-    if os.path.exists(CORES_FOLDER):
-        shutil.rmtree(CORES_FOLDER)
-    os.makedirs(CORES_FOLDER)
-    print "Re-created " + CORES_FOLDER
+    if os.path.exists(ts.CORES_FOLDER):
+        shutil.rmtree(ts.CORES_FOLDER)
+    os.makedirs(ts.CORES_FOLDER)
+    print "Re-created " + ts.CORES_FOLDER
 
 
 def compile_test_programs():
     """Invokes CMake and make to create the test binaries.
        Calls a debug build as I don't want optimizations to get in the way."""
-    subprocess.call(["cmake", "-DCMAKE_BUILD_TYPE=Debug", TEST_PROGRAMS_FOLDER], cwd=TEST_PROGRAMS_FOLDER)
-    subprocess.call(["make"], cwd=TEST_PROGRAMS_FOLDER)
+    subprocess.call(["cmake", "-DCMAKE_BUILD_TYPE=Debug", ts.TEST_PROGRAMS_FOLDER], cwd=ts.TEST_PROGRAMS_FOLDER)
+    subprocess.call(["make"], cwd=ts.TEST_PROGRAMS_FOLDER)
     print "All test programs compiled."
-
-
-def find_executables():
-    """Explores the conventional test programs folder to find all the executables in the cmake file. """
-    programs = []
-    expected_line_beginning = "add_executable("
-    expected_line_beginning_size = len(expected_line_beginning)
-    cmake_file = open(os.path.join(TEST_PROGRAMS_FOLDER, "CMakeLists.txt"))
-
-    for line in cmake_file:
-        if line.startswith(expected_line_beginning):
-            end_of_name = line.find(" ")
-            program_name = line[expected_line_beginning_size:end_of_name]
-            programs.append(program_name)
-            print "Detected test program " + program_name
-
-    return programs
 
 
 def run_and_core(executable):
     """Start the test program, then takes a core dump and kills it."""
-    test_program = subprocess.Popen([os.path.join(TEST_PROGRAMS_FOLDER, executable)])
+    test_program = subprocess.Popen([os.path.join(ts.TEST_PROGRAMS_FOLDER, executable)])
     pid = str(test_program.pid)
     time.sleep(1)  # Theoretically incorrect, but sufficient for our purposes.
-    subprocess.call(["sudo", "gcore", "-o", os.path.join(CORES_FOLDER, executable), pid])
+    subprocess.call(["sudo", "gcore", "-o", os.path.join(ts.CORES_FOLDER, executable), pid])
     subprocess.call(["kill", pid])
     print "Created core file for " + executable
 
@@ -66,6 +47,6 @@ def run_and_core(executable):
 if __name__ == "__main__":
     clean_up_core_folder()
     compile_test_programs()
-    executables = find_executables()
+    executables = ts.find_executables()
     for program in executables:
         run_and_core(program)
---
+++ TestSuite.py
@@ -0,0 +1,24 @@
+# Here we have some common code to handle the full-stack test cases.
+
+import os
+
+
+CORES_FOLDER = os.path.abspath("testCores")
+TEST_PROGRAMS_FOLDER = os.path.abspath("testPrograms")
+
+
+def find_executables():
+    """Explores the conventional test programs folder to find all the executables in the cmake file. """
+    programs = []
+    expected_line_beginning = "add_executable("
+    expected_line_beginning_size = len(expected_line_beginning)
+    cmake_file = open(os.path.join(TEST_PROGRAMS_FOLDER, "CMakeLists.txt"))
+
+    for line in cmake_file:
+        if line.startswith(expected_line_beginning):
+            end_of_name = line.find(" ")
+            program_name = line[expected_line_beginning_size:end_of_name]
+            programs.append(program_name)
+            print "Detected test program " + program_name
+
+    return programs
